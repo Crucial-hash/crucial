712bafb7fe2ccacc3d959d4bc66e04a93282553b
Yusuf Khimji <dab-marmalade-goes@duck.com>
2025-12-03 15:37:13 +0000

    added mobile testing adjusted resolution for desktop

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,6 +9,16 @@ export default defineConfig({
     video: 'on',
     trace: 'on',
   },
+  projects: [
+    {
+      name: 'desktop-16-9',
+      use: { viewport: { width: 1920, height: 1057 } }, // 1080p minus typical browser chrome
+    },
+    {
+      name: 'mobile-narrow',
+      use: { viewport: { width: 390, height: 844 } }, // common phone size to trigger column layout
+    },
+  ],
   webServer: {
     command: 'npx http-server . -p 3000 -c-1 --ext html',
     port: 3000,
